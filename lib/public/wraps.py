5e500932b381084251b40c2489620c648e01c707
xiaoxiaolulu <546464268@qq.com>
2018-12-14 18:04:57 +0800

    add email templates

--- lib/public/wraps.py
+++ lib/public/wraps.py
@@ -1,4 +1,6 @@
 # -*- coding:utf-8 -*-
+import json
+import builtins
 from functools import wraps
 from lib.public import logger
 from lib.public import http_keywords
@@ -6,42 +8,42 @@ from lib.utils.use_mysql import ExecuteSQL
 from lib.public.Recursion import GetJsonParams
 from lib.public.case_manager import TestContainer
 
-# ES = ExecuteSQL()
-#
-#
-# def test_data_runner(func):
-#
-#     @wraps(func)
-#     def wrap(*args, **kwargs):
-#
-#         for item in iter(ExecuteSQL.loads_sql_data()):
-#
-#             func_name = func.__name__.title().replace('_', '')
-#             if item['classname'] == func_name:
-#                 logger.log_debug("操作的数据库表为 ====> {}".format(item['classname']))
-#                 columns = ','.join(item['columns']) if len(item['columns']) else '*'
-#                 table = item['table']
-#                 params = item['params']
-#                 desc = item['desc']
-#                 if item['action'] == 'SELECT':
-#                     sql = 'SELECT {} FROM {} WHERE {} {}'.format(columns, table, params, desc)
-#                     result = ES.execute(sql)
-#                     logger.log_debug("执行的SQL语句为 ===> {}".format(sql))
-#                     logger.log_debug("执行结果为 ===> {}".format(result))
-#                 if item['action'] == 'DELETE':
-#                     sql = 'DELETE FROM {} WHERE {}'.format(table, params)
-#                     result = ES.execute(sql)
-#                     logger.log_debug("执行的SQL语句为 ===> {}".format(sql))
-#                     logger.log_debug("执行结果为 ===> {}".format(result))
-#         return func(*args, **kwargs)
-#
-#     return wrap
+ES = ExecuteSQL()
+
+
+def test_data_runner(func):
+
+    @wraps(func)
+    def wrap(*args, **kwargs):
+
+        for item in iter(ExecuteSQL.loads_sql_data()):
+
+            func_name = func.__name__.title().replace('_', '')
+            if item['classname'] == func_name:
+                logger.log_debug("操作的数据库表为 ====> {}".format(item['classname']))
+                columns = ','.join(item['columns']) if len(item['columns']) else '*'
+                table = item['table']
+                params = item['params']
+                desc = item['desc']
+                if item['action'] == 'SELECT':
+                    sql = 'SELECT {} FROM {} WHERE {} {}'.format(columns, table, params, desc)
+                    result = ES.execute(sql)
+                    logger.log_debug("执行的SQL语句为 ===> {}".format(sql))
+                    logger.log_debug("执行结果为 ===> {}".format(result))
+                if item['action'] == 'DELETE':
+                    sql = 'DELETE FROM {} WHERE {}'.format(table, params)
+                    result = ES.execute(sql)
+                    logger.log_debug("执行的SQL语句为 ===> {}".format(sql))
+                    logger.log_debug("执行结果为 ===> {}".format(result))
+        return func(*args, **kwargs)
+
+    return wrap
 
 
 def cases_runner(func):
 
     @wraps(func)
-    def wrap(*args, **kwargs):
+    def wrap(*args):
 
         for items in iter(TestContainer()):
             for key, value in dict(items).items():
@@ -55,8 +57,6 @@ def cases_runner(func):
                         kwassert=items.get('body').get('assert')
                     )
 
-        return func(*args, **kwargs)
-
     return wrap
 
 
@@ -67,22 +67,33 @@ def result_assert(func):
 
         response = kwargs.get('response')
         kwassert = kwargs.get('kwassert')
-
-        temp = tuple(kwargs.keys())
-        resassert = GetJsonParams.for_keys_to_dict(*temp, my_dict=response)
-
+        tmp = tuple(kwassert.keys())
+        result = GetJsonParams.for_keys_to_dict(*tmp, my_dict=response)
         for key, value in kwassert.items():
-            pass
+            if isinstance(value, list):
+                tp, _value = value
+                if tp == "type" and key == "responseType":
+                    result[key] = [tp, repr(getattr(builtins, tp)(response)).split("'")[1]]
+                elif tp == "type":
+                    result[key] = [tp, repr(getattr(builtins, tp)(result.get(key))).split("'")[1]]
+                else:
+                    result[key] = [tp, getattr(builtins, tp)(result.get(key))]
+
+        expect_assert_value = json.dumps(
+            result,
+            sort_keys=True,
+            ensure_ascii=False
+        )
+        kwassert_value = json.dumps(
+            kwassert,
+            sort_keys=True,
+            ensure_ascii=False
+        )
+        return func(*args, response=result, expect_assert_value=expect_assert_value, kwassert_value=kwassert_value)
+
+    return wrap
 
 
 if __name__ == '__main__':
-    @cases_runner
-    def add_channel(*args, **kwargs):
-        print(kwargs)
-        print(kwargs.get('response'))
-        print(kwargs.get('kwassert'))
+    pass
 
-    # {'code': 1, 'message': '此名称已存在。', 'data': None}
-    # {'code': 1, 'message': '此名称已存在。', 'data': 'None'}
-    # dict_keys(['code', 'message', 'data'])
-    add_channel()
