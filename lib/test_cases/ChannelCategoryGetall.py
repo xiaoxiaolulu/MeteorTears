ce3006a96579fdfe212e50bc3105537ef6b189a5
xiaoxiaolulu <546464268@qq.com>
2018-12-18 15:05:53 +0800

    add email templates

---
+++ lib/test_cases/ChannelCategoryGetall.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+import unittest
+from lib.public import wraps
+
+
+class ChannelCategoryGetall(unittest.TestCase):
+
+    """ChannelCategoryGetall接口测试脚本"""
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @wraps.cases_runner
+    @wraps.result_assert
+    def test_channel_category_getall(self, *args, **kwargs):
+        """获取渠道分类"""
+        response = kwargs.get('response')
+        self.assertEqual(kwargs.get('expect_assert_value'), kwargs.get('kwassert_value'))
