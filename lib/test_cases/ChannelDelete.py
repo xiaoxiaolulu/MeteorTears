d97388e9742ae967b1204d84059c63d616d38504
xiaoxiaolulu <546464268@qq.com>
2018-12-20 15:51:37 +0800

    add email templates

---
+++ lib/test_cases/ChannelDelete.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+import unittest
+from lib.public import wraps
+
+
+class ChannelDelete(unittest.TestCase):
+
+    """ChannelDelete接口测试脚本"""
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @wraps.cases_runner
+    @wraps.result_assert
+    def test_channel_delete(self, *args, **kwargs):
+        """删除渠道数据"""
+        response = kwargs.get('response')
+        self.assertEqual(kwargs.get('expect_assert_value'), kwargs.get('kwassert_value'))
---
+++ lib/test_cases/ChannelGetlist.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+import unittest
+from lib.public import wraps
+
+
+class ChannelGetlist(unittest.TestCase):
+
+    """ChannelGetlist接口测试脚本"""
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @wraps.cases_runner
+    @wraps.result_assert
+    def test_channel_getlist(self, *args, **kwargs):
+        """获取渠道列表数据"""
+        response = kwargs.get('response')
+        self.assertEqual(kwargs.get('expect_assert_value'), kwargs.get('kwassert_value'))
